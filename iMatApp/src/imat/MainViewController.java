92e31eaea8797619c2c433f23b76578daca834e5
Aurit1 <78141676+Aurit1@users.noreply.github.com>
2024-05-08 13:43:42 +0200

    Header added (WIP)

--- iMatApp/src/imat/MainViewController.java
+++ iMatApp/src/imat/MainViewController.java
@@ -7,12 +7,19 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.PopupWindow;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 public class MainViewController implements Initializable {
 
     @FXML
     Label pathLabel;
+    @FXML
+    AnchorPane MainHeader;
+    ImageView headerCart;
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
@@ -24,4 +31,10 @@ public class MainViewController implements Initializable {
 
     }
 
+
+    @FXML
+    public void MouseCartEnter(){
+        headerCart.setImage(new Image(getClass().getClassLoader().getResourceAsStream(
+                "logo_hover.png")));
+    }
 }
